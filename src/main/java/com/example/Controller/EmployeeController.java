353a14fb4286f7d06e67ca3db31b7de7f8582fb3
Veeranji49 <veeranjijlkr49@gmail.com>
2024-03-07 21:36:56 +0530

    added remaining operations

--- src/main/java/com/example/Controller/EmployeeController.java
+++ src/main/java/com/example/Controller/EmployeeController.java
@@ -3,10 +3,9 @@ package com.example.Controller;
 import com.example.Entity.Employee;
 import com.example.Service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/api/v1/employee")
@@ -28,5 +27,31 @@ public class EmployeeController {
             msg="failed in insertion";
         }
         return msg;
+    }
+
+    @PutMapping(value="/update/{id}")
+    public String updatee(@RequestBody Employee employee, @PathVariable long id)
+    {
+        employeeService.updateEmployee(employee,id);
+        return "Updated Successfully";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deletee(@PathVariable long id)
+    {
+        employeeService.deleteEmployee(id);
+        return "deleted successfully";
+    }
+
+    @GetMapping(value="/getone/{id}")
+    public Employee getOne(@PathVariable long id)
+    {
+        return employeeService.getOneEmployee(id);
+    }
+
+    @GetMapping(value="/getall")
+    public List<Employee> getall()
+    {
+        return employeeService.getAllEmployees();
     }
 }
