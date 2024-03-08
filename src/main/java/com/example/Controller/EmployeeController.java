60c2fd1c3c45f6f960eae84bdc58c287c946ff00
Veeranji49 <veeranjijlkr49@gmail.com>
2024-03-08 21:28:49 +0530

    added swagger annotations

--- src/main/java/com/example/Controller/EmployeeController.java
+++ src/main/java/com/example/Controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.example.Controller;
 
 import com.example.Entity.Employee;
 import com.example.Service.EmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,12 +12,21 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/api/v1/employee")
+@Api(tags = "Employee Controller", description = "To checking APIs")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
+    @GetMapping(value="/msg")
+    @ApiOperation("Get a greeting message")
+    public String msg()
+    {
+        return "Welcome to SpringBoot REST API Application";
+    }
+
     @PostMapping(value="/save")
+    @ApiOperation("To posting the employee details")
     public String savee(@Valid @RequestBody Employee employee)
     {
         Employee emp=employeeService.saveEmployee(employee);
@@ -31,6 +42,7 @@ public class EmployeeController {
     }
 
     @PutMapping(value="/update/{id}")
+    @ApiOperation("To updating the employee details")
     public String updatee(@Valid @RequestBody Employee employee, @PathVariable long id)
     {
         employeeService.updateEmployee(employee,id);
@@ -38,6 +50,7 @@ public class EmployeeController {
     }
 
     @DeleteMapping("/delete/{id}")
+    @ApiOperation("To deleting the employee details")
     public String deletee(@PathVariable long id)
     {
         employeeService.deleteEmployee(id);
@@ -45,12 +58,14 @@ public class EmployeeController {
     }
 
     @GetMapping(value="/getone/{id}")
+    @ApiOperation("getting the one employee details")
     public Employee getOne(@PathVariable long id)
     {
         return employeeService.getOneEmployee(id);
     }
 
     @GetMapping(value="/getall")
+    @ApiOperation("Getting all the employee details")
     public List<Employee> getall()
     {
         return employeeService.getAllEmployees();
